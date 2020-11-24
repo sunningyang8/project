e854e2bdde7eea4709bf9637d149da7f0b04549a
sunningyang8 <896047224@qq.com>
2020-11-24 11:23:44 +0800

    提交

---
+++ FourthWeek/day1/src/api/index.js
@@ -0,0 +1,11 @@
+// 关于用户的接口
+export const user = {
+  LoginUser: "login", // 用户登录
+  GetUsers: "users", // 获取所有用户
+  AddUser: "users" // 添加一个新用户
+};
+
+// 关于权限的接口
+export const rights = {
+  AsideMenus: "menus" // 获取左侧菜单权限
+};
